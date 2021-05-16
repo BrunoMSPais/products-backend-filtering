e7f769ef5cf01eca8eb3389d80612c79f6adb856
Bruno <bpais@outlook.pt>
2021-05-16 21:32:49 +0100

    Handeling filtering and sorting on the backend

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -3,7 +3,46 @@ const asyncHandler = require('../middleware/asyncHandler');
 const ErrorResponse = require('../utils/errorResponse');
 
 exports.getAllProducts = asyncHandler(async (req, res, next) => {
-  const products = await Product.find();
+  // Queries for filtering the data from the db
+  let query;
+
+  const reqQuery = { ...req.query };
+
+  const removeFields = ['sort', 'page'];
+
+  removeFields.forEach((val) => delete reqQuery[val]);
+
+  let queryString = JSON.stringify(reqQuery);
+
+  queryString = queryString.replace(
+    /\b(gt|gte|lt|lte|in)\b/g,
+    (match) => `$${match}`,
+  );
+
+  // End of filtering queries
+
+  // Sorting products
+  query = Product.find(JSON.parse(queryString));
+
+  if (req.query.sort) {
+    const sortByArr = req.query.sort.split(
+      ',',
+    ); /* wen doing the sort on the request the sorting query params will be separated by ','
+    like '.../products?sort=price,-rating' */
+
+    const sortByStr = sortByArr.join(
+      ' ',
+    ); /* joins the previous created array values in a string that matches the mongoDB sintaxe */
+
+    query = query.sort(sortByStr);
+  } else {
+    query = query.sort('-price'); /* default sorting -> by descending price */
+  }
+  // End of sorting products
+
+  // const products = await Product.find();
+  const products = await query; /* replaces the previous commented line to retrieve the data from mongoDB
+  according to the query instead of 'Product.find()' */
 
   res.status(200).json({
     success: true,
